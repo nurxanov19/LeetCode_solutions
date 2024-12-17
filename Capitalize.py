3545be25da854e35ef8d0f1e93bcd384cf894901
nurxanov19 <shoxjaxonnurxonov14@gmail.com>
2024-12-17 10:03:01 +0500

    Capitalize and Mutability masala bajarildi

---
+++ Capitalize.py
@@ -0,0 +1,4 @@
+def capitalize_word(word: str) -> str:
+    return word.title()
+
+print(capitalize_word('hello'))
--- name_shuffle.py
+++
@@ -1,5 +0,0 @@
-def name_shuffler(str_):
-    return ' '.join([val for val in str_.split(' ')[::-1]])
-
-
-print(name_shuffler("john McClane"))
